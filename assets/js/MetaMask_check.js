4898571b9eeb6ba419ad394d2dd214b69aa00321
amchercashin <amchercashin@gmail.com>
2017-11-15 15:49:59 +0300

    back

--- assets/js/MetaMask_check.js
+++ assets/js/MetaMask_check.js
@@ -1,20 +1,5 @@
 
-function MetaMask_load() {
 
-    // Checking if Web3 has been injected by the browser (Mist/MetaMask)
-    if (typeof web3 !== 'undefined') {
-        // Use Mist/MetaMask's provider
-        window.web3 = new Web3(web3.currentProvider);
-    } else {
-        //console.log('No web3? You should consider trying MetaMask!')
-        // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-        //window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-        document.getElementById("MetaMask_warning").innerHTML = 
-        `
-        <p><strong>Warning</strong> Install MetaMask Chrome addon and connect to your account.</p>
-        `
-    }      
-}
 
 function MetaMask_check() {
     if (typeof web3.eth.accounts[0] == 'undefined') {
