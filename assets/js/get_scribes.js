95e51df5277f92b917ef00b8200f99551aaf953a
amchercashin <amchercashin@gmail.com>
2017-11-16 18:34:29 +0300

    get_scribes

--- assets/js/get_scribes.js
+++ assets/js/get_scribes.js
@@ -4,46 +4,58 @@ function get_scribes() {
     var tablet_address = document.getElementById("settings_tablet").value;
     var tablet_instance = get_tablet_instance(tablet_address);
 
-    tablet_instance.tablet_length.call(function call_records_count(error, records_count) {
+    tablet_instance.scribes_hisory_length.call(function call_scribes_hisory_count(error, scribes_count) {
         if (!error) {
-            var records_table_html = 
+            var scribes_table_html = 
                 `
-                <table id="records_table">
+                <table id="scribes_table">
                     <tr>
                         <th class="row-1 row-ID">№</th>
-                        <th class="row-2 row-text">Record</th>
+                        <th class="row-2 row-text">Scribe</th>
                     </tr>                        
                 `
-                console.log("records_count " + records_count)
+            console.log("Scribe_history_count " + scribes_count)
 
-                var r;
-                var retrieved_records_count = 0;
-                for (r = 0; r < records_count; r++) {
-                    (function (r) {
-                        console.log(r);
-                        tablet_instance.records.call(r, function call_records(error, record) {
-                            if (!error) {
-                                console.log(record);
-                                var new_record_html = `
-                                <tr>
-                                    <td>` + eval(r + 1) + `</td>
-                                    <td>` + record + `</td>
-                                </tr>
-                                `
-                                records_table_html = records_table_html + new_record_html;
-                                document.getElementById("my_records").innerHTML = records_table_html + " </table>";
-                                retrieved_records_count = retrieved_records_count + 1;
-                                if (retrieved_records_count == records_count) {
-                                    console.log("calling sort");
-                                    sort_table(document.getElementById("records_table"));
+            var r;
+            var retrieved_scribes_count = 0;
+            for (r = 0; r < scribes_count; r++) {
+                (function (r) {
+                    console.log(r);
+                    tablet_instance.scribes_hisory.call(r, function call_scribes(error, scribe) {
+                        if (!error) {
+                            tablet_instance.scribes.call(scribe, function check_scribe(error, is_scribe) {
+                                console.log(scribe);
+                                if (is_scribe) {
+                                    var new_scribe_html = `
+                                    <tr>
+                                        <td>` + eval(r + 1) + `</td>
+                                        <td>` + scribe + `</td>
+                                    </tr>
+                                    `
+                                    scribes_table_html = scribes_table_html + new_scribe_html;
+                                    document.getElementById("scribes").innerHTML = scribes_table_html + " </table>";
+                                    retrieved_scribes_count = retrieved_scribes_count + 1;
+                                    if (retrieved_scribes_count == scribes_count) {
+                                        console.log("calling sort");
+                                        sort_table(document.getElementById("scribes_table"));
+                                    }
+
+                                } else {
+                                    scribes_count = scribes_count - 1;
+                                    if (retrieved_scribes_count == scribes_count) {
+                                        console.log("calling sort");
+                                        sort_table(document.getElementById("scribes_table"));
+                                    }
                                 }
-                            }
-                            else {
-                                console.log(error);
-                            }
-                        });
-                    })(r);
-                }
+
+                            })
+                        }
+                        else {
+                            console.log(error);
+                        }
+                    });
+                })(r);
+            }
         } else {
             console.log(error);
         }
