12ccb01e3b58997a05d4d572c6eff62a22026981
amchercashin <amchercashin@gmail.com>
2017-11-16 15:04:46 +0300

    tips and settings

--- assets/js/make_tablet.js
+++ assets/js/make_tablet.js
@@ -3,8 +3,11 @@ function make_tablet() {
 
     var tablet_factory_instance = get_tablet_factory_instance(document.getElementById("tablet_factory").value);
     var desired_tablet_name = document.getElementById("desired_tablet_name").value
-    
-    tablet_factory_instance.create_tablet(desired_tablet_name, {from:web3.eth.accounts[0], to:tablet_factory, value:""},
+    var tip = 0;
+    if (document.getElementById("tip").checked) {
+        tip = document.getElementById("tip_value").value;
+    }
+    tablet_factory_instance.create_tablet(desired_tablet_name, {from:web3.eth.accounts[0], to:tablet_factory, value:tip},
         function(error, tx_create_tablet){
             if(!error) {
                 document.getElementById("new_tablet_address").innerHTML = "pending table creation";
