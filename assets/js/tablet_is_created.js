abb61fd95ec458c7965e1e4a430ce19fa6c7c72b
amchercashin <amchercashin@gmail.com>
2017-11-16 11:16:14 +0300

    typo

--- assets/js/tablet_is_created.js
+++ assets/js/tablet_is_created.js
@@ -2,8 +2,8 @@ function tablet_is_created(tx_create_tablet, tablet_listener) {
     web3.eth.getTransactionReceipt(tx_create_tablet, function(error, receipt) {
         if(!error) {
             console.log("receipt: " + receipt);
-            if (receipt.status) {
-                document.getElementById("new_tablet_address").innerHTML = result.logs[0].address;
+            if (!receipt) {
+                document.getElementById("new_tablet_address").innerHTML = receipt.logs[0].address;
                 document.getElementById("new_tablet_address").className = "";
                 clearInterval(tablet_listener);
             }
