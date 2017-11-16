40a36eb2beb18e796657ff388b67a966a2f9636b
amchercashin <amchercashin@gmail.com>
2017-11-16 12:52:43 +0300

    pendind records add

--- assets/js/add_record.js
+++ assets/js/add_record.js
@@ -4,9 +4,27 @@ function add_record() {
         var tablet_address = document.getElementById("tablet").value;
         var tablet_instance = get_tablet_instance(tablet_address);
         tablet_instance.add_record(document.getElementById("record_to_add").value, {from:web3.eth.accounts[0], to:tablet_factory, value:""},
-            function add_record(error, record_tx) {
+            function add_record(error, tx_record) {
                 if (!error) {
-                    console.log(record_tx);
+                    document.getElementById("add_record_result").innerHTML = "pending record add";
+                    document.getElementById("add_record_result").className = "pending";
+                    console.log("Add record tx: " + tx_record);
+                    var record_listener = setInterval(
+                        function() {
+                            web3.eth.getTransactionReceipt(tx_record, function(error, receipt) {
+                                if(!error) {
+                                    console.log("receipt: " + receipt);
+                                    if (receipt) {
+                                        document.getElementById("new_tablet_address").innerHTML = "record has been added!";
+                                        document.getElementById("new_tablet_address").className = "";
+                                        clearInterval(record_listener);
+                                    }
+                                } else {
+                                    console.error(error);
+                                }
+                            })                                                        
+                        }
+                    , 10000);    
                 } else {
                     console.log(error);
                 }
