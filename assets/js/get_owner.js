27c22dd2624699a965ee457d6191c6d3fd5de06a
amchercashin <amchercashin@gmail.com>
2017-11-16 15:12:09 +0300

    get_owner

---
+++ assets/js/get_owner.js
@@ -0,0 +1,12 @@
+function get_owner() {
+    var tablet_address = document.getElementById("settings_tablet").value;
+    var tablet_instance = get_tablet_instance(tablet_address);
+    tablet_instance.tablet_owner.call(function owner_getter(error, owner) {
+            if (!error) {
+                document.getElementById("tablet_owner").innerHTML = owner;
+            } else {
+                console.log(error);
+            }
+        }
+    );
+}
--- assets/js/wallet_address_click.js
+++ assets/js/wallet_address_click.js
@@ -1,4 +1,5 @@
 function wallet_address_click(address) {
     document.getElementById("tablet").scrollIntoView();
     document.getElementById("tablet").value = address.innerHTML;
+    document.getElementById("settings_tablet").value = address.innerHTML;
 }
