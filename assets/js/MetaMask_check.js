7ff62fd520dad665de1a01f6e77d15ce5b9cb1bd
amchercashin <amchercashin@gmail.com>
2017-11-17 14:22:16 +0300

    Firefox

--- assets/js/MetaMask_check.js
+++ assets/js/MetaMask_check.js
@@ -2,7 +2,7 @@ function MetaMask_check() {
     if (typeof web3.eth.accounts[0] == 'undefined') {
         document.getElementById("MetaMask_warning").innerHTML = 
         `
-        <p><strong>&nbsp;Warning</strong> Install MetaMask Chrome addon and connect to your account.</p>
+        <p><strong>&nbsp;Warning</strong> Install MetaMask Chrome/Firefox addon and connect to your account.</p>
         `
     } else {
         window.web3 = new Web3(web3.currentProvider);
