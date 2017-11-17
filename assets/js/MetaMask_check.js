5c08086f07635bd6204a8d871737d964cb1d439f
amchercashin <amchercashin@gmail.com>
2017-11-17 13:58:54 +0300

    Revert "check"
    This reverts commit b903a6a9a8c4e565e7a5b18a8f3dd60b046923fd.

--- assets/js/MetaMask_check.js
+++ assets/js/MetaMask_check.js
@@ -1,5 +1,5 @@
 function MetaMask_check() {
-    if (typeof web3 == 'undefined' || typeof web3.eth.accounts[0] == 'undefined') {
+    if (typeof web3.eth.accounts[0] == 'undefined') {
         document.getElementById("MetaMask_warning").innerHTML = 
         `
         <p><strong>&nbsp;Warning</strong> Install MetaMask Chrome addon and connect to your account.</p>
