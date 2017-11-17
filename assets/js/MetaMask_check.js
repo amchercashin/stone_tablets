7d551ebeb2b0c423b0ae29f3880b876ecc007a5f
amchercashin <amchercashin@gmail.com>
2017-11-17 11:07:27 +0300

    Revert "web3.eth.getAccounts( workaround"
    This reverts commit 2942b5337caf839bc6399f75c3814b6151e833bd.

--- assets/js/MetaMask_check.js
+++ assets/js/MetaMask_check.js
@@ -1,14 +1,12 @@
 function MetaMask_check() {
-    web3.eth.getAccounts(function (error, acc) {
-        if (typeof acc == 'undefined') {
-            document.getElementById("MetaMask_warning").innerHTML = 
-            `
-            <p><strong>&nbsp;Warning</strong> Install MetaMask Chrome addon and connect to your account.</p>
-            `
-        }
+    if (typeof web3.eth.accounts[0] == 'undefined') {
+        document.getElementById("MetaMask_warning").innerHTML = 
+        `
+        <p><strong>&nbsp;Warning</strong> Install MetaMask Chrome addon and connect to your account.</p>
+        `
+    }
 
-        if (typeof acc == global_active_account) {
-            global_active_account = acc;
-        }
-    });
+    if (typeof web3.eth.accounts[0] !== global_active_account) {
+        global_active_account = web3.eth.accounts[0];
+    }
 }
